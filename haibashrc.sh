010ff5285af0c3e75b0734ed418626f886653eb2
Hai Nguyen <hai@mayfieldrobotics.com>
2015-03-13 11:16:38 -0700

    added tmux conf and debug to cmake gen for eclipse (needed to run things in eclipse)

--- haibashrc.sh
+++ haibashrc.sh
@@ -4,7 +4,7 @@ export SVN_EDITOR='vim'
 export EDITOR='vim'
 export HGMERGE='meld'
 export PS1='\[\033[01;32m\]\u@\h \[\033[01;34m\]\W \[\033[0;31m\]$(__git_ps1 "(%s)")\[\033[01;34m\]\$ \[\033[00m\]'
-export ROS_IP=`/sbin/ifconfig eth0 | grep "inet addr" | head -n 1 | cut -d ':' -f 2 | cut -d ' ' -f 1`
+export ROS_IP=`/sbin/ifconfig wlan0 | grep "inet addr" | head -n 1 | cut -d ':' -f 2 | cut -d ' ' -f 1`
 export ROS_MASTER_URI=http://$ROS_IP:11311
 
 alias ls="ls -FGksh --color=always"
@@ -13,6 +13,7 @@ alias python='python -u'
 alias v="gvim"
 alias sim="export ROBOT=sim"
 
-alias catkin_make_eclipse='cmake ../src/ -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_INSTALL_PREFIX=../install -DCATKIN_DEVEL_PREFIX=../devel'
+#alias catkin_make_eclipse='cmake ../src/ -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_INSTALL_PREFIX=../install -DCATKIN_DEVEL_PREFIX=../devel'
+alias catkin_make_eclipse='cmake ../src/ -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_INSTALL_PREFIX=../install -DCATKIN_DEVEL_PREFIX=../devel -DCMAKE_BUILD_TYPE=Debug'
 alias catkin_make_xcode='cmake ../src/ -G Xcode -DCMAKE_INSTALL_PREFIX=../install -DCATKIN_DEVEL_PREFIX=../devel'
 
