3c00d1820c37cb81cd4e558bbb30fe722b584f8f
Hai Nguyen <haidai@gmail.com>
2015-09-09 12:47:33 -0700

    made ROSIP smarter

--- haibashrc.sh
+++ haibashrc.sh
@@ -11,9 +11,6 @@ export EDITOR='vim'
 export HGMERGE='meld'
 export PS1='\[\033[01;32m\]\u@\h \[\033[01;34m\]\W \[\033[0;31m\]$(__git_ps1 "(%s)")\[\033[01;34m\]\$ \[\033[00m\]'
 
-#export ROS_IP=`/sbin/ifconfig en0 | grep "inet " | awk '{print $2}'`
-export ROS_IP=`dig +short $(hostname)`
-export ROS_MASTER_URI=http://$ROS_IP:11311
 
 alias ls="ls -FGksh"
 alias ..="cd .. && ls"
@@ -24,7 +21,7 @@ alias git_reveal='find . -type d -name ".git" -print -exec git --git-dir={} bran
 
 mkdir -p ~/.ssh
 mkdir -p ~/environment
-export DOCKER_DEV="-v $(abs_path ~/.ssh):/root/.ssh -v $(abs_path ~/environment):/root/environment -v $(abs_path ~/.bashrc):/root/.bashrc ~/$(abs_path ~/.vimrc):/root/.vimrc"
+export DOCKER_DEV="-v $(abs_path ~/.ssh):/root/.ssh -v $(abs_path ~/environment):/root/environment -v $(abs_path ~/.bashrc):/root/.bashrc -v $(abs_path ~/.vimrc):/root/.vimrc"
 
 if [ `uname` == "Darwin" ] ; then
     alias catkin_make_xcode='cmake ../src/ -G Xcode -DCMAKE_INSTALL_PREFIX=../install -DCATKIN_DEVEL_PREFIX=../devel'
@@ -36,5 +33,19 @@ if [ `uname` == "Darwin" ] ; then
     source ~/ros_catkin_ws/install_isolated/setup.bash
 else
     alias catkin_make_eclipse='cmake ../src/ -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_INSTALL_PREFIX=../install -DCATKIN_DEVEL_PREFIX=../devel -DCMAKE_BUILD_TYPE=Debug'
+    #export ROS_IP=`/sbin/ifconfig en0 | grep "inet " | awk '{print $2}'`
+    if ! type "dig" > /dev/null; then
+        sudo apt-get install -y dnsutils
+    fi
 fi
 
+export ROS_IP=`dig +short $(hostname)`
+if [ -z $ROS_IP ] ; then
+    echo "Failed to set ROS_IP with dig. Trying ip addr."
+    export ROS_IP=`ip addr | grep 'state UP' -A2 | grep "inet " | awk '{print $2}' | cut -f1 -d'/'`
+fi
+if [ -z $ROS_IP ] ; then
+    echo "ROS_IP is empty!"
+fi
+
+export ROS_MASTER_URI=http://$ROS_IP:11311
